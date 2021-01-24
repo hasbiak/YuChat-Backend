fe8c57a11f9d62d10f74226a370478bfda215a27
Hasbi Alwi Kusmana <67422750+hasbiak@users.noreply.github.com>
2021-01-25 01:14:14 +0700

    Add Mysql

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -4,6 +4,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  timeout: process.env.DB_TIME,
 });
 
 connection.connect((error) => {
