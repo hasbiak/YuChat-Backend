9daeddeb494c54b89ba06a3a05700345dbac585a
Hasbi Alwi Kusmana <67422750+hasbiak@users.noreply.github.com>
2021-02-18 18:07:10 +0700

    Update Static uploads

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use("/yuchat/fileuploadsyuchat", express.static("uploads"));
 app.use(express.static("uploads"));
 app.use((request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
